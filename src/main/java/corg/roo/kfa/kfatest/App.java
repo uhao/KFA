482e8bed8f29415de910ff244bcf3f390014e862
yhao <yhao@192.168.2.109>
2016-12-14 00:10:26 +0800

    init the project

---
+++ src/main/java/corg/roo/kfa/kfatest/App.java
@@ -0,0 +1,13 @@
+package corg.roo.kfa.kfatest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/corg/roo/kfa/kfatest/ProducerDemo.java
@@ -0,0 +1,9 @@
+package corg.roo.kfa.kfatest;
+import org.apache.kafka.clients.producer.*;
+public class ProducerDemo {
+
+	public static void main(String[] args){
+		
+		
+	}
+}
